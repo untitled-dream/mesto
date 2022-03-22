3eda74be7ed5f0c9aa7fe4bd6c0d00f93b84b17a
untitled-dream <untitled_dream@inbox.ru>
2022-03-22 22:50:21 +0300

    first itt

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,38 @@
+let editForm = document.getElementById("popup-edit");
+
+let nameCurrent = document.querySelector(".profile__name").innerText;
+let descriptionCurrent = document.querySelector(".profile__description").innerText;
+
+/* form opening */
+let openFormButton = document.querySelector(".profile__button-edit");
+function formOpenHandler(evt) {
+    evt.preventDefault();
+    editForm.classList.add("popup_opened");
+    document.getElementById("name").value = nameCurrent;
+    document.getElementById("description").value = descriptionCurrent;
+}
+openFormButton.addEventListener("click", formOpenHandler);
+
+/*  form closing */
+let closeFormButton = document.querySelector(".popup__button-close");
+function formCloseHandler(evt) {
+    evt.preventDefault();
+    editForm.classList.remove("popup_opened");
+}
+closeFormButton.addEventListener("click", formCloseHandler);
+
+/* form submit */
+let formElement = document.querySelector(".popup__button-submit");
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    let nameInput = document.getElementById("name").value;
+    let descriptionInput = document.getElementById("description").value;
+    document.querySelector(".profile__name").textContent = nameInput;
+    document.querySelector(".profile__description").textContent = descriptionInput;
+    editForm.classList.remove("popup_opened");
+}
+formElement.addEventListener("click", formSubmitHandler);
+
 function set_like() {
     let card_buttons_like = document.querySelectorAll(".elements .elements__card");
     let tab = [];
@@ -14,16 +49,3 @@ function set_like() {
         };
      }
 }
-
-function openEditForm() {
-    let editForm = document.getElementById("popup-edit");
-    editForm.classList.add("popup_opened");
-
-    document.getElementById("name").value = document.querySelector(".profile__name").innerText;
-    document.getElementById("description").value = document.querySelector(".profile__description").innerText;
-}
-
-function closeEditForm() {
-    let editForm = document.getElementById("popup-edit");
-    editForm.classList.remove("popup_opened");
- }
