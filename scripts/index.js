d375a8b860696ae89a8319d8b8672fa06013fe0a
untitled-dream <untitled_dream@inbox.ru>
2022-04-06 23:41:27 +0300

    Add new card & delete card

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,47 @@
 const profilePopup = document.querySelector("#profile-edit");
 const profileForm = document.forms["profile-edit-form"];
 
+const addCardPopup = document.querySelector("#card-add");
+const addCardForm = document.forms["card-add-form"];
+
 const openProfileFormButton = document.querySelector(".profile__button-edit");
-const closeProfileFormButton = document.querySelector(".popup__button-close");
+const openAddCardFormButton = document.querySelector(".profile__button-add");
 
 const nameInput = document.getElementById("profile-name");
 const descriptionInput = document.getElementById("profile-description");
 
+let cardNameInput = document.getElementById("card-name");
+let cardSourceInput = document.getElementById("card-source");
+
 const nameCurrent = document.querySelector(".profile__name");
 const descriptionCurrent = document.querySelector(".profile__description");
 
-/* default cards */ 
-const cardsList = document.querySelector('.elements__list');
+const cardsList = document.querySelector(".elements__list");
 const cardsTemplate = document.querySelector("#card-template").content;
 const defaultCards = [
     {
-      name: 'Казанский кремль',
-      source: './images/photo-grid-kazan.jpg'
+      name: "Казанский кремль",
+      source: "./images/photo-grid-kazan.jpg"
     },
     {
-      name: 'МГУ, Москва',
-      source: './images/photo-grid-moscow-2.jpg'
+      name: "МГУ, Москва",
+      source: "./images/photo-grid-moscow-2.jpg"
     },
     {
-      name: 'о. Эгина, Греция',
-      source: './images/photo-grid-aegina.jpg'
+      name: "о. Эгина, Греция",
+      source: "./images/photo-grid-aegina.jpg"
     },
     {
-      name: 'Куршская Коса',
-      source: './images/photo-grid-kurshskaya-kosa.jpg'
+      name: "Куршская Коса",
+      source: "./images/photo-grid-kurshskaya-kosa.jpg"
     },
     {
-      name: 'Москва',
-      source: './images/photo-grid-moscow-1.jpg'
+      name: "Москва",
+      source: "./images/photo-grid-moscow-1.jpg"
     },
     {
-      name: 'Афины',
-      source: './images/photo-grid-athens.jpg'
+      name: "Афины",
+      source: "./images/photo-grid-athens.jpg"
     }
   ];
 
@@ -47,38 +52,71 @@ const defaultCards = [
     cardElement.querySelector(".elements__image").src = card.source;
     cardElement.querySelector(".elements__image").alt = card.name;
 
-    cardElement.querySelector('.elements__button-like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle("elements__button-like_active");
-    });
-
+    cardElement.querySelector(".elements__button-like").addEventListener('click', setLikeHandler);
+    cardElement.querySelector(".elements__button-trash").addEventListener("click", deleteCardHandler);
+    
     cardsList.appendChild(cardElement);  
   })
 
-/* profile form opening */
+
 function formOpenHandler(evt) {
     evt.preventDefault();
-    profilePopup.classList.add("popup_opened");
     
-    nameInput.value = nameCurrent.textContent;
-    descriptionInput.value = descriptionCurrent.textContent;
+    if (evt.target.classList.value === 'profile__button-edit') {
+        profilePopup.classList.add("popup_opened");
+    
+        nameInput.value = nameCurrent.textContent;
+        descriptionInput.value = descriptionCurrent.textContent;
+    } else if (evt.target.classList.value === 'profile__button-add') {
+        addCardPopup.classList.add("popup_opened");
+        cardNameInput.value = '';
+        cardSourceInput.value = '';
+    }
 }
 
-/* form submit */
-function formSubmitHandler(evt) {
+function profileFormSubmit(evt) {
     evt.preventDefault();
     
     nameCurrent.textContent = nameInput.value;
     descriptionCurrent.textContent = descriptionInput.value;
-    
-    formCloseHandler(evt);
+
+    closeForm(evt);
 }
 
-/* form closing */
-function formCloseHandler(evt) {
+function addCardformSubmit(evt) {
     evt.preventDefault();
-    profilePopup.classList.remove("popup_opened");
+    const newCardElement = cardsTemplate.cloneNode(true);
+    if (!cardNameInput.value)
+        alert();
+    newCardElement.querySelector(".elements__title").textContent = cardNameInput.value;
+    newCardElement.querySelector(".elements__image").src = cardSourceInput.value;
+    newCardElement.querySelector(".elements__image").alt = cardNameInput.value;
+
+    newCardElement.querySelector(".elements__button-like").addEventListener('click', setLikeHandler);
+    newCardElement.querySelector(".elements__button-trash").addEventListener("click", deleteCardHandler);
+    
+    cardsList.prepend(newCardElement);
+    closeForm(evt);
 }
+
+function setLikeHandler(evt) {
+    evt.target.classList.toggle("elements__button-like_active");
+}
+ 
+function deleteCardHandler(evt) {
+    evt.target.closest(".elements__card").remove();
+}
+
+function closeForm(evt) {
+    evt.target.closest(".popup").classList.remove("popup_opened");
+}
+
+document.querySelectorAll(".popup__button-close").forEach(closeButton =>
+    closeButton.addEventListener("click", () => closeButton.closest(".popup").classList.remove("popup_opened"))
+)
 
 openProfileFormButton.addEventListener("click", formOpenHandler);
-profileForm.addEventListener("submit", formSubmitHandler);
-closeProfileFormButton.addEventListener("click", formCloseHandler);
+openAddCardFormButton.addEventListener("click", formOpenHandler);
+
+profileForm.addEventListener("submit", profileFormSubmit);
+addCardForm.addEventListener("submit", addCardformSubmit);
