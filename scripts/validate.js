686d38c2979c747dd7663b19331a691241d02a1a
untitled-dream <untitled_dream@inbox.ru>
2022-04-19 16:18:44 +0300

    Validate JS

---
+++ scripts/validate.js
@@ -0,0 +1,47 @@
+/*const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add('form__input_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__input-error_active');
+};
+  
+ 
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('form__input_type_error');
+    errorElement.classList.remove('form__input-error_active');
+    errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+*/
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup'));
+    formList.forEach((formElement) => {
+      formElement.addEventListener('submit', (evt) => {
+       evt.preventDefault();
+      });
+      
+      const fieldsetList = Array.from(formElement.querySelectorAll('.popup__fieldset'));
+      
+      fieldsetList.forEach((fieldset) => {
+       setEventListeners(fieldset);
+      });
+    })
+  };
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
