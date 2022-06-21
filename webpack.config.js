8b00483b6c45d7f87593baa56c19de290b34a16b
Smirnov Andrey <untitled_dream@inbox.ru>
2022-06-21 11:23:27 +0300

    fix/favicon

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                test: /\.(png|ico|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
                 type: 'asset/resource'
             },
             {
@@ -38,7 +38,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            inject: 'head'
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
